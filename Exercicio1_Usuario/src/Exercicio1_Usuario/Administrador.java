4a94af1e65609a54de745cca41c1006a3405d82b
Giovanna <mennezes.giovanna@gmail.com>
2026-06-26 17:12:40 -0300

    feat: classes de usuario

---
+++ Exercicio1_Usuario/src/Exercicio1_Usuario/Administrador.java
@@ -0,0 +1,24 @@
+package Exercicio1_Usuario;
+
+public class Administrador extends Usuario{
+	int nivelDeAcesso;
+
+	public Administrador(String nome, String email, int nivelDeAcesso) {
+		super(nome, email);
+		this.nivelDeAcesso = nivelDeAcesso;
+	}
+
+	public int getNivelDeAcesso() {
+		return nivelDeAcesso;
+	}
+
+	public void setNivelDeAcesso(int nivelDeAcesso) {
+		this.nivelDeAcesso = nivelDeAcesso;
+	}
+	
+	@Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Nível de acesso: " + this.nivelDeAcesso);
+	}
+}
